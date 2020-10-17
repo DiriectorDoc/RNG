4be59be6eb0824a83ee6a7378318b9aad44b6277
DiriectorDoc <66105586+DiriectorDoc@users.noreply.github.com>
2020-10-16 20:00:28 -0400

    Still a work in progress

--- RNG.js
+++ RNG.js
@@ -7,37 +7,48 @@
 		root.RNG = factory(root.RNG);
 	}
 }(typeof window !== "undefined" ? window : this, function(RNG){
+	
+	let abs = x => (x ^ (x >> 31)) - (x >> 31);
 
 	RNG = function(seed){
-		// LCG using GCC's constants
-		this.m = 0x80000000; // 2**31;
+		this.m = 0x80000000;
 		this.a = 1103515245;
 		this.c = 12345;
-
-		this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+		
+		switch(typeof seed){
+			case "number":
+				this.state = seed;
+				break;
+			case "string":
+				if(/^[a-zA-Z0-9\s]*$/g.test(seed)){
+					this.state = 0n;
+					for(let i = 0; i < seed.substr(0, 32).length; i++){
+						let char = seed.charCodeAt(i) - 48;
+						if(char > 42)
+							char -= 32;
+						if(char > 9)
+							char -= 7;
+						if(char == -16)
+							char = 37;
+						this.state += BigInt(char)*37n**BigInt(i)
+					}
+					this.state = +(BigInt.asIntN(54, this.state)+"");
+					break;
+				}
+			default:
+				this.state = Math.floor(Date.now() * (this.m - 1))
+		}
 	}
 	RNG.prototype.nextInt = function(){
-		return this.state = (this.a * this.state + this.c) % this.m;
+		return this.state = abs((this.a * this.state + this.c) % this.m);
 	}
-	RNG.prototype.nextSafeInt = function(){
-		let numStr = Math.round(this.nextFloat()) ? "" : "-",
-			unsafe = true,
-			s = "900719925474099".split("").push(numStr ? "2":"1");
-		for(let i = 0; i < 16; i++){
-			if(unsafe){
-				let nextChar = this.nextRange(0, +s[i]+1);
-				if(nextChar < .s[i]){
-					numStr += nextChar;
-					unsafe = false
-				} else {
-					numStr += s[i]
-				}
-			} else {
-				numStr += this.nextRange(0, 10)
-			}
+	RNG.prototype.nextSafeInt = function(allowNegatives){
+		let numStr = "";
+		for(let i = 0; i < 54; i++){
+			numStr += this.nextInt();
 		}
 		
-		return this.state;
+		return this.state = +(BigInt.asIntN(54, numStr)+"");
 	}
 	RNG.prototype.nextFloat = function(){
 		// returns in range [0,1]
