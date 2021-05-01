d7cdb0f659c70d3f59df200c86ef68f946c4f2ea
Diriector_Doc <davijust213@yahoo.ca>
2021-04-30 23:56:57 -0400

    Overhauled

--- RNG.js
+++ RNG.js
@@ -1,68 +1,68 @@
-(function(root, factory){
-	if (typeof define === "function" && define.amd) {
-		define(["RNG"], function(a){
-			return (root.amdWebGlobal = factory(a))
-		})
-	} else {
-		root.RNG = factory(root.RNG)
-	}
-}(typeof window !== "undefined" ? window : this, function(RNG){
-	
-	let abs = x => (x ^ (x >> 31)) - (x >> 31),
-		int = b => +(BigInt.asIntN(54, b)+"");
+module.exports = (function(){
+	class RNG {
 
-	RNG = function(seed){
-		this.m = 0x80000000;
-		this.a = 1103515245;
-		this.c = 12345;
-		
-		switch(typeof seed){
-			case "number":
-				this.state = seed;
-				break;
-			case "string":
-				if(/^[a-zA-Z0-9\s]*$/g.test(seed)){
-					this.state = 0n;
-					for(let i = 0; i < seed.substr(0, 32).length; i++){
-						let char = seed.charCodeAt(i) - 48;
-						if(char > 42)
-							char -= 32;
-						if(char > 9)
-							char -= 7;
-						if(char == -16)
-							char = 37;
-						this.state += BigInt(char)*37n**BigInt(i)
+		#state;
+
+		static #stateGenerator = function*(state) {
+			let abs = x => (x ^ (x >> 31)) - (x >> 31);
+			while (true) {
+				yield state = abs(1103515245 * state + 12345) % 0x80000000
+			}
+		}
+
+		constructor(seed){
+			switch(typeof seed){
+				case "number":
+					this.#state = RNG.#stateGenerator(seed);
+					break;
+				case "string":
+					if(/^[a-zA-Z0-9\s]*$/g.test(seed)){
+						let state = 0n;
+						for(let i = 0; i < seed.substr(0, 32).length; i++){
+							let char = seed.charCodeAt(i) - 48;
+							if(char > 42)
+								char -= 32;
+							if(char > 9)
+								char -= 7;
+							if(char == -16)
+								char = 37;
+							this.#state += BigInt(char)*37n**BigInt(i)
+						}
+						this.#state = RNG.#stateGenerator(+(BigInt.asIntN(54, state)+""));
+						break
 					}
-					this.state = int(this.state);
-					break
-				}
-			default:
-				this.state = Math.floor(Date.now() * (this.m - 1))
+				default:
+					this.#state = RNG.#stateGenerator((Date.now() * 0x7fffffff)|0)
+			}
 		}
-	}
-	RNG.prototype.nextInt = function(){
-		return this.state = abs(this.a * this.state + this.c) % this.m
-	}
-	RNG.prototype.nextSafeInt = function(allowNegatives){
-		let numStr = "";
-		for(let i = 0; i < 54; i++){
-			numStr += this.nextInt()
+
+		get nextInt(){
+			return this.#state.next().value
 		}
-		
-		return this.state = int(numStr)
-	}
-	RNG.prototype.nextFloat = function(){
-		// returns in range [0,1)
-		return this.nextInt() / (this.m - 1)
-	}
-	RNG.prototype.nextRange = function(start, end){
-		// returns in range [start, end): including start, excluding end
-		return start + Math.floor(this.nextInt() * (end - start) / this.m)
-	}
-	RNG.prototype.choice = function(array){
-		let arr = array instanceof Array && arguments.length == 1 ? array : arguments;
-		return arr[this.nextRange(0, arr.length)]
+
+		get nextSafeInt(){
+			let numStr = "";
+			for(let i = 0; i < 54; i++){
+				numStr += this.nextInt
+			}
+			return this.#state = +(BigInt.asIntN(54, numStr)+"")
+		}
+
+		get nextFloat(){
+			// returns in range [0,1)
+			return this.nextInt / 0x7fffffff
+		}
+
+		nextRange(start, end){
+			// returns in range [start, end): including start, excluding end
+			return start + (this.nextInt * (end - start) / 0x80000000)|0
+		}
+
+		choice(array){
+			let arr = array instanceof Array && arguments.length == 1 ? array : arguments;
+			return arr[this.nextRange(0, arr.length)]
+		}
 	}
 
 	return RNG
-}))
+})()
